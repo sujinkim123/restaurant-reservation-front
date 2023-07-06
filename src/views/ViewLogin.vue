4f99d57f453eb5fd3b51b9659692c7ef7288674c
sujinkim <92345910+comes043@users.noreply.github.com>
2023-07-06 17:29:19 +0900

    로그인 화면 틀 생성

--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,9 +1,20 @@
+<!-- https://onethejay.tistory.com/128 -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <p>
-    로그인 페이지입니다.
-    </p>
+    <h2>Please Log In</h2>
+    <div id="loginForm">
+      <form>
+        <p>
+          <input class="w3-input" name="uid" placeholder="아이디를 입력해주세요" v-model="user_id"><br>
+        </p>
+        <p>
+          <input name="password" class="w3-input" placeholder="비밀번호를 입력해주세요" v-model="user_pw" type="password">
+        </p>
+        <p>
+          <button type="submit" class="w3-button w3-green w3-round">로그인</button>
+        </p>
+      </form>
+    </div>
   </div>
 </template>
 
