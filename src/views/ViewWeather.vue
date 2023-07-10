3fb0f03c17efd7ccbf3a094352ad8b6f37455b1d
sujinkim <92345910+comes043@users.noreply.github.com>
2023-07-10 11:24:59 +0900

    날씨 페이지 임시커밋

--- src/views/ViewWeather.vue
+++ src/views/ViewWeather.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-    <div id="map" style="width:500px; height:600px;">
-    <p>오늘의 날씨</p>
-    <p>주간날씨</p>
-    <p>날씨검색</p>
-  </div>
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="검색..."/>
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northampton, UK</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9℃</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -15,6 +25,14 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: 'imcreSoojin';
+  src:url('../assets/font/ImcreSoojin.ttf')
+}
+@font-face {
+  font-family: 'korta-hope';
+  src: url('../assets/font/KOTRA HOPE.ttf');
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +40,70 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding : 15px;
+
+  color: #313131;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box.location {
+  color : #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box.date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: imcreSoojin;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: imcreSoojin;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
